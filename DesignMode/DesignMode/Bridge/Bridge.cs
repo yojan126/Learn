e633ec11b3ce2c961da029319a8f6ba89109efca
yojan126 <48440759+yojan126@users.noreply.github.com>
2019-03-25 10:30:15 +0800

    Bridge Mode
    桥接模式

---
+++ DesignMode/DesignMode/Bridge/Bridge.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/******************************桥接模式**************************************/
+namespace Bridge
+{
+    /// <summary>
+    /// 以电视机遥控器的例子来演示桥接模式
+    /// </summary>
+    public class Bridge
+    {
+        static void Main()
+        {
+            // 创建一个遥控器
+            RemoteControl remoteControl = new RemoteControl();
+
+            // 长虹电视机
+            remoteControl.Implementor = new Changhong();
+            remoteControl.On();
+            remoteControl.SetChannel(1);
+            remoteControl.Off();
+
+            // 三星电视机
+            remoteControl.Implementor = new Samsung();
+            remoteControl.On();
+            remoteControl.SetChannel(2);
+            remoteControl.Off();
+
+        }
+    }
+
+    /// <summary>
+    /// 抽象概念中的遥控器，扮演抽象化角色
+    /// </summary>
+    public class RemoteControl
+    {
+        /// <summary>
+        /// 字段
+        /// </summary>
+        private TV implementor;
+
+        /// <summary>
+        /// 属性
+        /// </summary>
+        public TV Implementor
+        {
+            get { return implementor; }
+            set { implementor = value; }
+        }
+
+        /// <summary>
+        /// 打开电视，这里抽象类中不提供实现了，而是调用实现类中的实现
+        /// </summary>
+        public virtual void On()
+        {
+            implementor.On();
+        }
+
+        /// <summary>
+        /// 关闭电视
+        /// </summary>
+        public virtual void Off()
+        {
+            implementor.Off();
+        }
+
+        /// <summary>
+        /// 环频道
+        /// </summary>
+        /// <param name="i"></param>
+        public virtual void SetChannel(int i)
+        {
+            implementor.tuneChannel(i);
+        }
+    }
+
+    /// <summary>
+    /// 电视机，提供抽象方法
+    /// </summary>
+    public abstract class TV
+    {
+        public abstract bool On();
+        public abstract bool Off();
+        public abstract int tuneChannel(int CurrentChannel);
+    }
+
+    /// <summary>
+    /// 长虹电视机，重写基类的抽象方法
+    /// 提供具体的实现
+    /// </summary>
+    public class Changhong : TV
+    {
+        public override bool On()
+        {
+            return true;
+        }
+
+        public override bool Off()
+        {
+            return false;
+        }
+
+        public override int tuneChannel(int CurrentChannel)
+        {
+            return CurrentChannel + 1;
+        }
+    }
+
+    /// <summary>
+    /// 三星电视机，重写基类的抽象方法
+    /// </summary>
+    public class Samsung : TV
+    {
+        public override bool On()
+        {
+            return true;
+        }
+
+        public override bool Off()
+        {
+            return false;
+        }
+
+        public override int tuneChannel(int CurrentChannel)
+        {
+            return CurrentChannel - 1;
+        }
+    }
+}
---
+++ DesignMode/DesignMode/Bridge/BridgeFramwork.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/***********************桥接模式（三层架构原始模型）**************************/
+namespace Bridge
+{
+    public class BridgeFramwork
+    {
+    }
+
+
+    public class Client
+    {
+        static void Main()
+        {
+            BusinessObject customers = new CustomersBusinessObject("shanghai");
+            customers.DataAccess = new CustomersDataAccess();
+
+            customers.Add("Jame");
+            customers.GetAll();
+            customers.Delete("Tom");
+            customers.GetAll();
+            customers.Update("Harry");
+            customers.Get(2);
+        }
+    }
+
+    /// <summary>
+    /// BLL层
+    /// </summary>
+    public class BusinessObject
+    {
+        /// <summary>
+        /// 字段
+        /// </summary>
+        private DataAccess dataAccess;
+        private string city;
+
+        public BusinessObject(string city)
+        {
+            this.city = city;
+        }
+
+        /// <summary>
+        /// 属性
+        /// </summary>
+        public DataAccess DataAccess
+        {
+            get { return dataAccess; }
+            set { dataAccess = value; }
+        }
+
+        /// <summary>
+        /// 方法
+        /// </summary>
+        /// <param name="name"></param>
+        public virtual void Add(string name)
+        {
+            DataAccess.AddRecord(name);
+        }
+
+        public virtual void Delete(string name)
+        {
+            DataAccess.DeleteRecord(name);
+        }
+
+        public virtual void Update(string name)
+        {
+            DataAccess.UpdateRecord(name);
+        }
+
+        public virtual void Get(int index)
+        {
+            dataAccess.GetRecord(index);
+        }
+
+        public virtual void GetAll()
+        {
+            DataAccess.ShowAllRecords();
+        }
+    }
+
+    public class CustomersBusinessObject : BusinessObject
+    {
+        public CustomersBusinessObject(string city)
+            : base(city) { }
+
+        /// <summary>
+        /// 重写方法
+        /// </summary>
+        public override void GetAll()
+        {
+            base.GetAll();
+        }
+    }
+
+    /// <summary>
+    /// 相当于三层架构中的数据访问层（DAL）
+    /// </summary>
+    public abstract class DataAccess
+    {
+        // 对记录的增删改查操作
+        public abstract void AddRecord(string name);
+        public abstract void DeleteRecord(string name);
+        public abstract void UpdateRecord(string name);
+        public abstract string GetRecord(int index);
+        public abstract void ShowAllRecords();
+    }
+
+    public class CustomersDataAccess : DataAccess
+    {
+        /// <summary>
+        /// 字段
+        /// </summary>
+        private List<string> customers = new List<string>();
+
+        public CustomersDataAccess()
+        {
+            // 实际业务中从数据库中读取数据再填充列表
+            customers.Add("Tom");
+            customers.Add("Jack");
+            customers.Add("Harry");
+        }
+
+        /// <summary>
+        /// 重写方法
+        /// </summary>
+        /// <param name="name"></param>
+        public override void AddRecord(string name)
+        {
+            customers.Add(name);
+        }
+
+        public override void DeleteRecord(string name)
+        {
+            customers.Remove(name);
+        }
+
+        public override void UpdateRecord(string name)
+        {
+            customers[0] = name;
+        }
+
+        public override string GetRecord(int index)
+        {
+            return customers[index];
+        }
+
+        public override void ShowAllRecords()
+        {
+            List<string> lst = new List<string>();
+            foreach (string str in customers)
+            {
+                lst.Add(str);
+            }
+        }
+    }
+}
