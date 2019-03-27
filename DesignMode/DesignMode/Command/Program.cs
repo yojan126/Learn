5e1c5495c55aa17c7fc8d7c07884b558d96af150
yojan126 <48440759+yojan126@users.noreply.github.com>
2019-03-27 10:12:42 +0800

    IteratorMode
    迭代器模式

--- DesignMode/DesignMode/Command/Program.cs
+++ DesignMode/DesignMode/Command/Program.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+/**************************命令模式************************************/
 namespace Command
 {
     class Program
---
+++ DesignMode/DesignMode/Iterator/Program.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/****************************迭代器模式***********************************/
+namespace Iterator
+{
+    /// <summary>
+    /// 客户端
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Iterator iterator;
+            IListCollection list = new ConcreteList();
+            iterator = list.GetIterator();
+
+            while (iterator.MoveNext())
+            {
+                int i = (int)iterator.GetCurrent();
+                Console.WriteLine(i.ToString());
+                iterator.Next();
+            }
+
+            Console.ReadLine();
+        }
+    }
+
+    /// <summary>
+    /// 抽象聚合类
+    /// </summary>
+    public interface IListCollection
+    {
+        Iterator GetIterator();
+    }
+
+    /// <summary>
+    /// 迭代器抽象类
+    /// </summary>
+    public interface Iterator
+    {
+        bool MoveNext();
+        Object GetCurrent();
+        void Next();
+        void Reset();
+    }
+
+    /// <summary>
+    /// 具体聚合类
+    /// </summary>
+    public class ConcreteList : IListCollection
+    {
+        int[] collection;
+        public ConcreteList()
+        {
+            collection = new int[] { 2, 4, 6, 8 };
+        }
+
+        public Iterator GetIterator()
+        {
+            return new ConcreteIterator(this);
+        }
+
+        public int Length
+        {
+            get { return collection.Length; }
+        }
+
+        public int GetElement(int index)
+        {
+            return collection[index];
+        }
+    }
+
+    /// <summary>
+    /// 具体迭代器类
+    /// </summary>
+    public class ConcreteIterator : Iterator
+    {
+        // 迭代器要对集合对象进行遍历操作，自然就需要引用集合对象
+        private ConcreteList _list;
+        private int _index;
+
+        public ConcreteIterator(ConcreteList list)
+        {
+            this._list = list;
+            this._index = 0;
+        }
+
+        public bool MoveNext()
+        {
+            if (_index < _list.Length)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public Object GetCurrent()
+        {
+            return _list.GetElement(_index);
+        }
+
+        public void Reset()
+        {
+            _index = 0;
+        }
+
+        public void Next()
+        {
+            if (_index < _list.Length)
+            {
+                _index++;
+            }
+        }
+    }
+}
