819244efda8c617848698bddb57e5cdf3ae2a376
JohnFeng <fengzh126@163.com>
2019-04-10 15:59:03 +0800

    StrategyMode
    策略者模式

---
+++ DesignMode/DesignMode/Strategy/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/**********************************策略者模式*******************************************/
+namespace Strategy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Strategy.DoThis();
+        }
+    }
+}
---
+++ DesignMode/DesignMode/Strategy/Strategy.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Strategy
+{
+    public class Strategy
+    {
+        public static void DoThis()
+        {
+            // 个人所得税方式
+            InterestOperation operation = new InterestOperation(new PersonalTaxStrategy());
+            Console.WriteLine("个人支付的税为：{0}", operation.GetTax(5000.00));
+
+            operation = new InterestOperation(new EnterpriseTaxStrategy());
+            Console.WriteLine("企业支付的税为：{0}", operation.GetTax(50000.00));
+
+            Console.ReadLine();
+        }
+    }
+
+    /// <summary>
+    /// 所得税计算策略
+    /// </summary>
+    public interface ITaxStrategy
+    {
+        double CalculateTax(double income);
+    }
+
+    /// <summary>
+    /// 个人所得税计算策略
+    /// </summary>
+    public class PersonalTaxStrategy : ITaxStrategy
+    {
+        public double CalculateTax(double income)
+        {
+            return income * 0.12;
+        }
+    }
+
+    /// <summary>
+    /// 企业所得税计算策略
+    /// </summary>
+    public class EnterpriseTaxStrategy : ITaxStrategy
+    {
+        public double CalculateTax(double income)
+        {
+            return (income - 3500) > 0 ? (income - 3000) * 0.045 : 0.00;
+        }
+    }
+
+    /// <summary>
+    /// 利益操作
+    /// </summary>
+    public class InterestOperation
+    {
+        private ITaxStrategy m_strategy;
+        public InterestOperation(ITaxStrategy taxStrategy)
+        {
+            this.m_strategy = taxStrategy;
+        }
+
+        public double GetTax(double income)
+        {
+            return m_strategy.CalculateTax(income);
+        }
+    }
+}
